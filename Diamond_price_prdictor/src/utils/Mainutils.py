0046a4e927bc00650145f7525766ce62dd92dd80
Nishchal Gaur <82520349+NK278@users.noreply.github.com>
2024-06-10 19:34:03 +0530

    Update Mainutils.py

--- Diamond_price_prdictor/src/utils/Mainutils.py
+++ Diamond_price_prdictor/src/utils/Mainutils.py
@@ -1,4 +1,4 @@
-import sys,os,pickle,yaml,boto3
+import sys,os,pickle,yaml
 from typing import Dict,Tuple
 import pandas as pd
 import numpy as np
@@ -12,6 +12,19 @@ class MainUtils:
     def __init__(self) -> None:
         pass
 
+
+    @staticmethod
+    def save_object(file_path,obj):
+        logging.info("Entered the save_object method of MainUtils class")
+        try:
+            dir_path=os.path.dirname(file_path)
+            os.makedirs(dir_path,exist_ok=True)
+
+            with open(file_path,'wb') as file_obj:
+                pickle.dump(obj,file_obj)
+        except Exception as e:
+            logging.info("error occured during saving!")
+
     @staticmethod
     def load_object(file_path:str)->object:
         logging.info('Entered the load_object method of mainutils class')
@@ -19,5 +32,17 @@ class MainUtils:
             with open(file_path,'rb') as file_obj:
                 return pickle.load(file_obj)
             
-        except Exception as e: raise CustomException(e,sys)
+        except Exception as e:
+             logging.info("error occured during loading!")
+             raise CustomException(e,sys)
+    
+    @staticmethod
+    def evaluate_model(true, predicted):
+        mae = mean_absolute_error(true, predicted)
+        mse = mean_squared_error(true, predicted)
+        rmse = np.sqrt(mean_squared_error(true, predicted))
+        r2_square = r2_score(true, predicted)
+        return mae, rmse, r2_square
+
+
 
