858b2b21cca1891bf334b86a0f056c19439b22a5
Nishchal Gaur <82520349+NK278@users.noreply.github.com>
2024-06-11 15:27:49 +0530

    Update data_ingestion.py

--- Diamond_price_prdictor/src/components/data_ingestion.py
+++ Diamond_price_prdictor/src/components/data_ingestion.py
@@ -1,6 +1,9 @@
 import os
 import sys
 from src.logger import logging
+from pymongo import MongoClient
+from zipfile import Path
+import numpy as np
 from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -16,11 +19,22 @@ class DataIngestionConfig:
 class DataIngestion:
     def __init__(self):
         self.data_ingestion_config=DataIngestionConfig()
+    
+    def export_col_as_df(self,col_name,db):
+        try:
+            mongo_client=MongoClient("mongodb+srv://nishchalgaur2003:zatfem-wymxyX-kyvqy3@cluster0.kvvgt6j.mongodb.net/?retryWrites=true&w=majority")
+            col=mongo_client[db][col_name]
+            df=pd.DataFrame(list(col.find()))
+            if "_id" in df.columns.to_list():
+                df=df.drop(columns=['_id'],axis=1)
+            df.replace({"na":np.nan},inplace=True)
+            return df
+        except Exception as e: raise CustomException(e,sys) from e
 
     def initiate_data_ingestion(self):
-        logging.info("Initiating Data Ingestion!")
         try:
-            df=pd.read_csv(os.path.join('notebooks\gemstone.csv','gemstone.csv'))
+            logging.info("Initiating Data Ingestion!")
+            df=self.export_col_as_df(db='ML_project1',col_name='DiamondPricePred')
             logging.info("Dataset read from dataframe")
             df.to_csv(self.data_ingestion_config.raw_data_path,index=False)
             logging.info("Train Test Split")
